6f3038a6fc1dcc1e7356f47b45ce33fca2a6c8f3
Victor Vinicius <victorvinicius@MacBook-Air-de-Victor.local>
2025-02-21 09:44:25 -0300

    att

--- src/data/Sicredi/DesafioSicredi.js
+++ src/data/Sicredi/DesafioSicredi.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import '../../layouts/Java/Crud.css';
+import '../../layouts/Sicredi/DesafioSicredi.css';
 
 const Crud = {
-  id: 'crud',
+  id: 'desafiosicredi',
   title: 'Desafio Sicredi',
   renderContent: () => {
-
     return (
-      <div class="crud-content">
+      <div className="desafiosicredi-content">
         <h1>
           Desafio <span className="gradient-text2">Sicredi</span>
         </h1>
-        <br></br>
-        </div>
+        <br />
+        <iframe 
+          src="https://db-api-lf1e.onrender.com/proxy-sicredi" 
+          title="Desafio Sicredi"
+          className="iframe-container"
+          allowFullScreen
+        />
+      </div>
     );
   },
 };
